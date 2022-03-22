f102a9772d8db73dfad529d808d14a839019cc42
qhuu2742 <qhuu.official@gmail.com>
2022-03-22 23:48:07 +0700

    Restructure the directory and adding remember login

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,32 +1,30 @@
 export default {
+
   async login(context, payload) {
-    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBE8kwb1RK-jEBFyy6rmsJkPNoSVBcGfCU', {
-      method: 'POST',
-      body: JSON.stringify({
-        email: payload.email,
-        password: payload.password,
-        returnSecureToken: true
-      })
-    });
-
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      console.log(responseData);
-      const error = new Error(responseData.message || 'Failed to authenticate.');
-      throw error;
-    }
-
-    console.log(responseData);
-    context.commit('setUser', {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login'
     });
   },
 
   async signup(context, payload) {
-    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBE8kwb1RK-jEBFyy6rmsJkPNoSVBcGfCU', {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup'
+    });
+  },
+
+
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBE8kwb1RK-jEBFyy6rmsJkPNoSVBcGfCU';
+
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBE8kwb1RK-jEBFyy6rmsJkPNoSVBcGfCU';
+    }
+    const response = await fetch(url, {
       method: 'POST',
       body: JSON.stringify({
         email: payload.email,
@@ -38,18 +36,38 @@ export default {
     const responseData = await response.json();
 
     if (!response.ok) {
-      console.log(responseData);
-      const error = new Error(responseData.message || 'Failed to authenticate.');
+      const error = new Error(
+        responseData.message || 'Failed to authenticate. Check your login data.'
+      );
       throw error;
     }
 
-    console.log(responseData);
+    // lưu trữ dữ liệu trên trình duyệt, local storage
+    localStorage.setItem('token', responseData.idToken);
+    localStorage.setItem('userId', responseData.localId);
+
     context.commit('setUser', {
       token: responseData.idToken,
       userId: responseData.localId,
       tokenExpiration: responseData.expiresIn
     });
   },
+
+  tryLogin(context) {
+    //truy cập vào dữ liệu trên trình duyệt
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+
+    // kiểm tra xem có dữ liệu trên trình duyệt không
+    if (token && userId) {
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+        tokenExpiration: null
+      });
+    }
+  },
+
   logout(context) {
     context.commit('setUser', {
       token: null,
