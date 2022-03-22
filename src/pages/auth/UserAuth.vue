0d41f5907809dff2d2c026e8b196facbe1811496
qhuu2742 <qhuu.official@gmail.com>
2022-03-22 17:13:04 +0700

    Adding routing for login button

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -82,10 +82,11 @@ export default {
       try {
         if (this.mode === 'login') {
           await this.$store.dispatch('login', actionPayload);
-          this.$router.replace('/coaches');
         } else {
           await this.$store.dispatch('signup', actionPayload);
         }
+        const redirectUrl = '/' + (this.$route.query.redirect || 'coaches');
+        this.$router.replace(redirectUrl);
       } catch (err) {
         this.error = err.message || 'Failed to authenticate!';
       }
